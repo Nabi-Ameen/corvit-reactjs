7075dbc04ff3cd717201104d960c107d7d1e758f
Nabi-Ameen <nabiameen35126@gmail.com>
2024-05-27 08:08:50 -0700

    discus useState

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
+import { useState } from "react";
 import Card from "./components/Card";
-import kfc1 from "./images/kfc1.png"
-import kfc2 from "./images/kfc2.png"
-import kfc3 from "./images/kfc3.png"
+import kfc1 from "./images/kfc1.png";
+import kfc2 from "./images/kfc2.png";
+import kfc3 from "./images/kfc3.png";
 import kfc4 from "./images/kfc4.png"
 
 function App() {
 
-  const products = [
-    {
-      id: 1,
-      image: kfc1,
-      title: "Twister Combo",
-      desc: "Twister + 1 Regular fries + 1 Regular drink",
-      price: "Rs 670"
-    },
-    {
-      id: 2,
-      image: kfc2,
-      title: "Zinger Stacker Combo",
-      desc: "Turn up the fun with 5 pcs Hot & Crispy",
-      price: "Rs 1290"
-    },
-    {
-      id: 3,
-      image: kfc3,
-      title: "Zinger Stacker Combo",
-      desc: "1 Zinger Stacker + 1 Regular fries + 1",
-      price: "Rs 890"
-    }
-  ]
+  // const products = [
+  //   {
+  //     id: 1,
+  //     image: kfc1,
+  //     title: "Twister Combo",
+  //     desc: "Twister + 1 Regular fries + 1 Regular drink",
+  //     price: "Rs 670"
+  //   },
+  //   {
+  //     id: 2,
+  //     image: kfc2,
+  //     title: "Zinger Stacker Combo",
+  //     desc: "Turn up the fun with 5 pcs Hot & Crispy",
+  //     price: "Rs 1290"
+  //   },
+  //   {
+  //     id: 3,
+  //     image: kfc3,
+  //     title: "Zinger Stacker Combo",
+  //     desc: "1 Zinger Stacker + 1 Regular fries + 1",
+  //     price: "Rs 890"
+  //   }
+  // ]
 
+  const [istoggle, setIsToggle] = useState(false);
   return (
-    <div className="flex">
+    <div>
 
+
+      <button onClick={() => setIsToggle(!istoggle)} className="bg-blue-800 text-white ml-40">show</button>
       {
+        istoggle ?
+          <div className="fixed inset-0 bg-black/45 flex justify-center items-center ">
+            <div className="w-96 h-96 bg-white ">
+              <button onClick={() => setIsToggle(!istoggle)}>Close</button>
+            </div>
+            {/* {
         products.map((product) => (
           <>
             <Card product={product}/>
           </>
         ))
-      }
+      } */}
 
-      {/* <Card image={kfc1} title="Twister Combo" desc="Twister + 1 Regular fries + 1 Regular drink" price="Rs 670" />
+            {/* <Card image={kfc1} title="Twister Combo" desc="Twister + 1 Regular fries + 1 Regular drink" price="Rs 670" />
       <Card image={kfc2} title="Zinger Stacker Combo" desc="1 Zinger Stacker + 1 Regular fries + 1" price="Rs 890" />
       <Card image={kfc3} title="Zinger Stacker Combo" desc="Turn up the fun with 5 pcs Hot & Crispy" price="Rs 1290" /> */}
+          </div> : <></>
+      }
     </div>
   );
 }
