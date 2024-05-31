b72e0d2c39b4b880e76c3965f121bc2a82aafb51
Nabi-Ameen <nabiameen35126@gmail.com>
2024-05-31 07:51:44 -0700

    form handling with object

--- src/components/FormHandling.jsx
+++ src/components/FormHandling.jsx
@@ -1,46 +1,87 @@
 import React, { useState } from 'react'
 
 const FormHandling = () => {
+    const [person, setPerson] = useState(
+        {
+            userName: "",
+            email: "",
+            gender: "",
+            qual: [],
+        }
+    )
 
-    const [userName, setUserName] = useState("Ameen");
-    const [email, setEmail] = useState("ameen@gmail.com");
-
-    const handleName = (e) => {
-        setUserName(e.target.value)
+    const handleQualification = (e) => {
+        const { value } = e.target
+        if (e.target.checked) {
+            setPerson({
+                ...person,
+                qual: [...person.qual, value]
+            })
+        } else {
+            setPerson({
+                ...person,
+                qual: person.qual.filter((qual) => qual !== value)
+            }
+            )
+        }
     }
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(userName, email)
+        console.log("person", person)
     }
-
-
 
     return (
-        <div className='flex justify-center mt-10'>
-            <form onSubmit={(e)=> handleSubmit(e)}>
-                <div className='w-1/2 space-y-4 '>
-                    <div>
-                        <label htmlFor="">User Name</label>
-                        <input type='text' id='' name='' value={userName} onChange={(e) => handleName(e)} className=' outline ml-3' />
-                    </div>
+        <form onSubmit={handleSubmit}>
+            <div className='w-1/2 m-10 border'>
+                <div>
+                    <label htmlFor="userName">User Name</label>
+                    <input type='text' id='userName' onChange={(e) => setPerson({ ...person, userName: e.target.value })} className='ml-4 border border-gray-700 w-96 p-2' />
+                </div>
 
+                <div>
+                    <label htmlFor="userName">Email</label>
+                    <input type='text' onChange={(e) => setPerson({ ...person, email: e.target.value })} id='Email' className='ml-14 mt-4 border border-gray-700 w-96 p-2' />
+                </div>
+
+                <div className='flex items-center mt-10 pl-20'>
                     <div>
-                        <label htmlFor="">User Email</label>
-                        <input type='text' id='' name='' value={email} className=' outline ml-3' onChange={(e) => handleEmail(e)} />
+                        <label htmlFor="male">Male</label>
+                        <input type="radio" value="Male" name='gender' onChange={(e) => setPerson({ ...person, gender: e.target.value })} className='ml-2' />
+                    </div>
+                    <div className='pl-10'>
+                        <label htmlFor="male">female</label>
+                        <input type="radio" value="female" name='gender' onChange={(e) => setPerson({ ...person, gender: e.target.value })} id='female' className='ml-2 ' />
                     </div>
                 </div>
 
+                <div className='flex items-center mt-10 pl-20'>
+                    <div>
+                        <label htmlFor="male">PHD</label>
+                        <input type="checkbox" value="PHD" onChange={handleQualification} className='ml-2' />
+                    </div>
+                    <div className='pl-10'>
+                        <label htmlFor="male">MS</label>
+                        <input type="checkbox" value="MS" onChange={handleQualification} className='ml-2' />
+                    </div>
 
-                <button onClick={handleSubmit} type='button' className='bg-black text-white p-2 rounded-md mt-10'>Submit</button>
-            </form>
+                    <div className='pl-10'>
+                        <label htmlFor="male">BS</label>
+                        <input type="checkbox" value="BS" onChange={handleQualification} className='ml-2' />
+                    </div>
 
+                    <div className='pl-10'>
+                        <label htmlFor="male">FA</label>
+                        <input type="checkbox" value="FA" onChange={handleQualification} className='ml-2' />
+                    </div>
 
-        </div>
+                </div>
+
+                <div>
+                    <button className='px-10 py-2 bg-gray-800 text-white rounded-md mt-10 ml-20'>Save</button>
+                </div>
+            </div>
+        </form>
     )
 }
 
