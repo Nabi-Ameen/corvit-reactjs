8d41a6b624b77f6ea47cc3babe0fadfbc62bd39a
Nabi-Ameen <nabiameen35126@gmail.com>
2024-06-10 07:42:37 -0700

    NavLink react router dom

--- src/App.js
+++ src/App.js
@@ -10,32 +10,43 @@ import FormHandling from "./components/FormHandling";
 import Form from "./components/Form";
 import FormikForm from "./components/formik/FormikForm";
 import FieldArrays from "./components/formik/FieldArray";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import Layout from "./components/Layout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  )
   return (
     <div>
-
-      
+      <RouterProvider router={router}/>
       {/* <FormHandling/>
       <Form/>
       <FormikForm/>
       <FieldArrays/> */}
 
-      <Router>
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Layout/>}>
             <Route path="/" element={<Home />} />
             <Route path="/aboutus" element={<AboutUs />} />
             <Route path="/contactus" element={<ContactUs />} />
+            <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
-      </Router>
+      </Router> */}
 
     </div>
   );
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      the routes you are looking is not found 404 page
+    </div>
+  )
+}
+
+export default PageNotFound
